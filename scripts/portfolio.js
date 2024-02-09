7a7f644230909d6cf836788b7148f378c8da254b
seankennelly <sean.kennelly@hotmail.co.uk>
2024-02-09 13:04:14 +0000

    Reconfigure style directory, add tile flip functionality to portfolio.js

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,5 +1,5 @@
+// HEADER ANIMATION
 const meImg = document.querySelector("#me-img");
-
 // Function passes 4 animations to header pic in a loop
 let animationIndex = 0;
 const animate = () => {
@@ -16,5 +16,29 @@ const animate = () => {
     animationIndex = 0;
   };
 }
-
 meImg.addEventListener('click', animate);
+
+// TILE FLIP ANIMATION
+const moonriseInfo = document.getElementById('moonrise-info');
+const moonriseTile = document.getElementById('moonrise-tile');
+const moonriseBack = document.getElementById('moonrise-back');
+
+const moonriseObj = 
+  {
+    info: moonriseInfo,
+    tile: moonriseTile,
+    back: moonriseBack
+  }
+;
+
+const flip = (obj) => { 
+  obj.tile.classList.toggle('flip-container') 
+};
+
+moonriseObj.info.addEventListener('click', () => {
+  flip(moonriseObj);
+});
+
+moonriseObj.back.addEventListener('click', () => {
+  flip(moonriseObj);
+});
